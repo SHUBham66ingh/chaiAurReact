be25f67140137fbc65e3e6b9392e90056c3a4fb9
Shubham singh <shubham1041singh@gmail.com>
2024-05-10 00:01:27 +0530

    customReact

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,12 @@
-
+import Chai from "./chai"
 function App() {
  
   return (
-   <h1>
-   hello react | shubham is here
-   </h1>
+    <>
+   <Chai/>
+   <h1>chai aur react</h1>
+   <p>test para</p>
+   </>
   )
 }
 
